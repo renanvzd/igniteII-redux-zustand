60ff688fd2466dcc58e118b24cfcf88662ef49fc
Renan Veronez Drechsler <renanvz@outlook.com>
2023-07-08 11:01:03 +0100

    First Commit

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState: ['Fazer cafe', 'Estudar Redux'],
+
+  reducers: {}
+})
+
+export const store = configureStore({
+  reducer: {
+    todo: todoSlice.reducer
+  }
+})

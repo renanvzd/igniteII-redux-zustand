cd8a117f48d89656af01e8d42da1526cdb18bc5f
Renan Veronez Drechsler <renanvz@outlook.com>
2023-07-09 12:29:56 +0100

    Reducer do Player

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { useSelector, TypedUseSelectorHook } from 'react-redux';
+import { useSelector, TypedUseSelectorHook } from "react-redux";
+import { player } from "./slices/player";
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    player
+  },
 })
 
-export type RootState = ReturnType<typeof store.getState>
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

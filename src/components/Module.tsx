fa9c2516900c4b2c9ab9c9a3630d9358a7c2f7a9
Renan Veronez Drechsler <renanvz@outlook.com>
2023-07-10 22:11:04 +0100

    Selecionando aula atual

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -3,6 +3,8 @@ import * as Collapsible from '@radix-ui/react-collapsible';
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
 import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { play } from "../store/slices/player";
 
 interface ModuleProps {
   moduleIndex: number;
@@ -11,6 +13,8 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+  const dispatch = useDispatch()
+
   const lessons = useAppSelector(state => {
     return state.player.course.modules[moduleIndex].lessons
   })
@@ -21,23 +25,21 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
         <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
           {moduleIndex + 1}
         </div>
-
         <div className="flex flex-col gap-1 text-left">
           <strong className="text-sm">{title}</strong>
           <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
         </div>
-
         <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
       </Collapsible.Trigger>
-
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson) => {
+          {lessons.map((lesson, lessonIndex) => {
             return (
               <Lesson
                 key={lesson.id}
                 title={lesson.title}
                 duration={lesson.duration}
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
               />
             )
           })}
--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -9,12 +9,12 @@ const playerSlice = createSlice({
           id: '1',
           title: 'Iniciando com React',
           lessons: [
-            { id: 'Jai8w6K_GnY', title: 'CSS Modules', duration: '13:45' },
-            { id: 'w-DW4DhDfcw', title: 'Estilização do Post', duration: '10:05' },
-            { id: 'D83-55LUdKE', title: 'Componente: Header', duration: '06:33' },
-            { id: 'W_ATsETujaY', title: 'Componente: Sidebar', duration: '09:12' },
-            { id: 'Pj8dPeameYo', title: 'CSS Global', duration: '03:23' },
-            { id: '8KBq2vhwbac', title: 'Form de comentários', duration: '11:34' },
+            { id: 'Jai8w6K_GnY', title: 'Introdução', duration: '13:45' },
+            { id: 'w-DW4DhDfcw', title: 'Ambiente de Desenvolv', duration: '10:05' },
+            { id: 'D83-55LUdKE', title: 'Váriaveis e tipos de dados', duration: '06:33' },
+            { id: 'W_ATsETujaY', title: 'Operações matemáticas', duration: '09:12' },
+            { id: 'Pj8dPeameYo', title: 'Funções', duration: '03:23' },
+            { id: '8KBq2vhwbac', title: 'Condicionais', duration: '11:34' },
           ],
         },
         {
@@ -28,9 +28,18 @@ const playerSlice = createSlice({
           ],
         },
       ],
+    },
+    currentModuleIndex: 0,
+    currentLessonIndex: 0,
+  },
+  reducers: {
+    play: (state, action) => {
+      state.currentModuleIndex = action.payload[0]
+      state.currentLessonIndex = action.payload[1]
     }
   },
-  reducers: {},
 })
 
 export const player = playerSlice.reducer;
+
+export const { play } = playerSlice.actions;

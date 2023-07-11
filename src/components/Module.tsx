177a9775ebd888efe54206554b6ec7158bcee1f1
Renan Veronez Drechsler <renanvz@outlook.com>
2023-07-11 09:50:31 +0100

    Carregando dados do curso

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -22,7 +22,7 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   })
 
   const lessons = useAppSelector(state => {
-    return state.player.course.modules[moduleIndex].lessons
+    return state.player.course?.modules[moduleIndex].lessons
   })
 
   return (
@@ -31,24 +31,28 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
         <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
           {moduleIndex + 1}
         </div>
+
         <div className="flex flex-col gap-1 text-left">
           <strong className="text-sm">{title}</strong>
           <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
         </div>
+
         <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
       </Collapsible.Trigger>
+
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson, lessonIndex) => {
-            const isCurrent = currentModuleIndex === moduleIndex && currentLessonIndex === lessonIndex
+          {lessons && lessons.map((lesson, lessonIndex) => {
+            const isCurrent = currentModuleIndex === moduleIndex &&
+              currentLessonIndex === lessonIndex
 
             return (
               <Lesson
                 key={lesson.id}
                 title={lesson.title}
                 duration={lesson.duration}
-                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
                 isCurrent={isCurrent}
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
               />
             )
           })}
